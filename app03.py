fa7bbf88138a63351ba2ba712cd9f4d670657d2f
hyesu33 <kdjsksjchjakqosid@gmail.com>
2025-05-07 22:54:59 +0900

    Update app03.py

--- app03.py
+++ app03.py
@@ -6,7 +6,7 @@ import io
 st.title("📘 English Quiz from CSV")
 
 # --- STEP 1: Load TSV (tab-separated) CSV from GitHub ---
-csv_url = "https://raw.githubusercontent.com/hyesuu33/myapps/main/quiz_questions.csv"  # use .tsv if applicable
+csv_url = "https://github.com/hyesu33/Myapps/blob/main/quize_questions.csv"  # use .tsv if applicable
 
 try:
     response = requests.get(csv_url)
